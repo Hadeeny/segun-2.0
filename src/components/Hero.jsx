eeafef92f5b151ea457c6446b2a92559e83c3b2c
hadeeny <oluwasegunadeniyi064@gmail.com>
2023-01-01 07:57:50 +0100

    entrance aniimation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,20 +6,27 @@ const Hero = () => {
     justify-center items-start gap-y-2 md:gap-y-6 mx-auto"
     >
       <motion.h2 
-      transition={{delay:8, duration:1}}
+      transition={{delay:9, duration:2.5}}
       initial={{opacity:0}}
       animate={{opacity:1}}
       className="text-primary text-2xl">Hi, my name is</motion.h2>
       <motion.div 
-      transition={{delay:8, duration:1}}
-      initial={{opacity:0}}
-      animate={{opacity:1}}
+      // transition={{delay:14, duration:1}}
+      // initial={{opacity:0}}
+      // animate={{opacity:1}}
       className="space-y-4 md:space-y-6">
-        <h1 className="text-white text-[2.1rem] leading-[2.2rem] md:text-[5rem] font-semibold">
+        <motion.h1 
+        transition={{delay:11.5, duration:1.5}}
+        initial={{opacity:0, y:20}}
+        animate={{opacity:1, y:0}}
+        className="text-white text-[2.1rem] leading-[2.2rem] md:text-[5rem] font-semibold">
           Oluwasegun Adeniyi
-        </h1>
+        </motion.h1>
 
-        <h2
+        <motion.h2
+        transition={{delay:5, duration:3}}
+        initial={{opacity:0, x:'100%'}}
+        animate={{opacity:1, x:0}}
           className="text-gray-200 text-[2.1rem] leading-[2.4rem] md:text-[5rem] 
         max-w-sm md:max-w-4xl md:leading-[5rem] font-semibold"
         >
@@ -29,22 +36,22 @@ const Hero = () => {
             creativity
           </span>{" "}
           is just the begining
-        </h2>
+        </motion.h2>
       </motion.div>
 
       <motion.p 
       
-      transition={{delay:8, duration:1}}
-      initial={{opacity:0}}
-      animate={{opacity:1}}
+      transition={{delay:11.8, duration:1}}
+      initial={{opacity:0, y:-50}}
+      animate={{opacity:1, y:0}}
       
       className="max-w-max text-gray-200 mt-6 md:max-w-xl text-xl">
         I’m a software engineer specialized in building fast, responsive & exceptional digital experiences
       </motion.p>
       <motion.div 
-      transition={{delay:8, duration:1}}
-      initial={{opacity:0}}
-      animate={{opacity:1}}
+      transition={{delay:11.8, duration:1}}
+      initial={{opacity:0, y:50}}
+      animate={{opacity:1, y:0}}
       className="leading-[7rem]">
         <a
           href="projects"
