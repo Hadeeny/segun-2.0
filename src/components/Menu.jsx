5d327a9a01c0abc7b588063670c7b875bd4ac102
hadeeny <oluwasegunadeniyi064@gmail.com>
2023-01-05 17:20:43 +0100

    progress

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { motion, AnimatePresence, delay } from "framer-motion";
 const Menu = () => {
   const [showMenu, setShowMenu] = useState(false);
   console.log(showMenu);
@@ -23,29 +23,56 @@ const Menu = () => {
       <div className="relative w-full h-full">
         <div
           className={`bg-leMon fixed top-0 left-0 z-[1200] w-1/4 duration-500 ${
-            showMenu ? "h-full" : "h-0"
+            showMenu ? "h-full" : "h-0 delay-[1200ms]"
           }`}
         ></div>
         <div
-          className={`bg-leMon duration-500 delay-[400ms] fixed top-0 left-[25%] right-0 z-[1200] w-1/4 ${
-            showMenu ? "h-full" : "h-0"
+          className={`bg-leMon duration-500 fixed top-0 left-[25%] right-0 z-[1200] w-1/4 ${
+            showMenu ? "h-full delay-[400ms]" : "h-0 delay-[800ms]"
           }`}
         ></div>
         <div
-          className={`bg-leMon duration-500 delay-[800ms] fixed top-0 left-[50%] right-0 z-[1200] w-1/4 ${
-            showMenu ? "h-full" : "h-0"
+          className={`bg-leMon duration-500  fixed top-0 left-[50%] right-0 z-[1200] w-1/4 ${
+            showMenu ? "h-full delay-[800ms]" : "h-0 delay-[400ms]"
           }`}
         ></div>
         <div
-          className={`bg-leMon duration-500 delay-[1200ms] fixed top-0 right-0 z-[1200] w-1/4 ${
-            showMenu ? "h-full" : "h-0"
+          className={`bg-leMon duration-500  fixed top-0 right-0 z-[1200] w-1/4 ${
+            showMenu ? "h-full delay-[1200ms]" : "h-0 delay-[100ms]"
           }`}
         ></div>
-        {/* <div className="bg-leMon fixed top-0 left-[50%] right-0 z-[1200] w-1/4 h-full"></div> */}
-        {/* <div className="bg-leMon fixed top-0 z-[1200] right-0 w-1/4 h-full"></div> */}
-        <div className="fixed hidden top-0 left-0 right-0 w-10/12 mx-auto bg-rouge z-[1300]">
-          Content
-        </div>
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0, x: 20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 2 }}
+            className={`fixed top-[25%] flex ${
+              !showMenu && "hidden"
+            } flex-col md:flex-row left-0 right-0 w-10/12 mx-auto z-[1300]`}
+          >
+            <div className="w-1/2">
+              <div className="mb-10 text-sm font-semibold uppercase">
+                Online
+              </div>
+            </div>
+            <div className="w-1/2">
+              <div className="mb-10 text-sm font-semibold uppercase">
+                Navigation
+              </div>
+              <ul className="space-y-4">
+                <li>
+                  <a className="text-[2rem] uppercase">Home</a>
+                </li>
+                <li>
+                  <a className="text-[2rem] uppercase">Project</a>
+                </li>
+                <li>
+                  <a className="text-[2rem] uppercase">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </motion.div>
+        </AnimatePresence>
       </div>
     </>
   );
