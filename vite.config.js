d9db7b13f27374ccfb53a3bedcfcb7063e834046
hadeeny <oluwasegunadeniyi064@gmail.com>
2023-01-13 13:43:54 +0100

    manifest

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,35 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
       devOptions: {
         enabled: true,
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Oluwasegun's Portfolio",
+        short_name: "Oluwasegun",
+        description:
+          "A self-taught Front End Developer specialized in building fast, responsive & exceptional digital experiences",
+        icons: [
+          {
+            src: "https://i.postimg.cc/HsznSBhb/segunsmall.png",
+            sizes: "32x32",
+            type: "image/webp",
+          },
+          {
+            src: "https://i.postimg.cc/Pqk7PQdF/segunbig.png",
+            sizes: "512x512",
+            type: "image/webp",
+          },
+        ],
+        start_url: "https://segun-adeniyi.vercel.app/",
+        display: "fullscreen",
+        theme_color: "#bae67e",
+        background_color: "#202838",
       },
     }),
   ],
